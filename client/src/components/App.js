b21ca76cb8a009339264db3dd40c8fe0364734e4
hojin9622 <kiss0104040@gmail.com>
2020-07-18 00:27:53 +0900

    Global & Navbar Styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+import NavBar from './NavBar';
 import Home from '../routes/Home';
 import Authors from '../routes/Authors';
 import Books from '../routes/Books';
@@ -8,19 +10,43 @@ import NewBook from '../routes/NewBook';
 import ViewAuthor from '../routes/ViewAuthor';
 import ViewBook from '../routes/ViewBook';
 
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;400;500;700;900&display=swap');
+
+  body {
+    font-family: 'Frank Ruhl Libre', serif;
+    font-size: 20px;
+  }
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  margin-bottom: 3rem;
+`;
+
+const InnerContainer = styled.div`
+  margin: 0 2rem;
+`;
+
 function App() {
   return (
     <Router>
-      <nav>Library</nav>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/authors' component={Authors} />
-        <Route exact path='/authors/new' component={NewAuthor} />
-        <Route exact path='/authors/:id' component={ViewAuthor} />
-        <Route exact path='/books' component={Books} />
-        <Route exact path='/books/new' component={NewBook} />
-        <Route exact path='/books/:id' component={ViewBook} />
-      </Switch>
+      <GlobalStyle />
+      <Container>
+        <NavBar />
+        <InnerContainer>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/authors' component={Authors} />
+            <Route exact path='/authors/new' component={NewAuthor} />
+            <Route exact path='/authors/:id' component={ViewAuthor} />
+            <Route exact path='/books' component={Books} />
+            <Route exact path='/books/new' component={NewBook} />
+            <Route exact path='/books/:id' component={ViewBook} />
+          </Switch>
+        </InnerContainer>
+      </Container>
     </Router>
   );
 }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem 0;
+`;
+
+const ListContainer = styled.ul`
+  display: flex;
+`;
+
+const List = styled.li``;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  padding: ${(props) => (props.title ? '' : '1rem')};
+  font-size: ${(props) => (props.title ? '3rem' : '1.8rem')};
+  font-weight: ${(props) => (props.title ? 'bold' : '300')};
+`;
+
+export default () => {
+  return (
+    <NavContainer>
+      <StyledLink to='/' title>
+        Library
+      </StyledLink>
+      <ListContainer>
+        <List>
+          <StyledLink to='/authors'>Authors</StyledLink>
+        </List>
+        <List>
+          <StyledLink to='/authors/new'>Add Author</StyledLink>
+        </List>
+        <List>
+          <StyledLink to='/books'>Books</StyledLink>
+        </List>
+        <List>
+          <StyledLink to='/authors/new'>Add Book</StyledLink>
+        </List>
+      </ListContainer>
+    </NavContainer>
+  );
+};
